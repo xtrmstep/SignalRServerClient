258f23171abf7a656be0865e29e8f69ca975a1a7
Alex <alexander.g@btigroup.io>
2020-10-29 14:01:24 +0200

    Added gRpc service

---
+++ GrpcService/NotificationsHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GrpcService
+{
+    public class NotificationsHub : Hub
+    {
+        public Task ShowMessage(string message)
+        {
+            return Clients.All.SendAsync("ShowMessage", message);
+        }
+        
+        public override async Task OnConnectedAsync()
+        {
+            Debug.WriteLine($"New connection: {Context.ConnectionId}");
+            await base.OnConnectedAsync();
+        }
+        
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            Debug.WriteLine($"Disconnecting: {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
